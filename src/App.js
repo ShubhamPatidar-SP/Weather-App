1d55b1a3ef362083da66c5d61de99052173a9971
ShubhamPatidar-SP <shubhampatidarshubh@gmail.com>
2023-10-08 01:02:26 +0530

    IMAGE UPDAbackground image setupTION

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import Forecast from "./components/forecast/forecast";
@@ -8,6 +8,44 @@ import "./App.css";
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const defaultCity = "Bhopal"; // Default city is set to "Bhopal"
+
+  useEffect(() => {
+    // Fetch weather data for the default city when the component mounts
+    const fetchData = async () => {
+      try {
+        // Fetch current weather data
+        const currentWeatherResponse = await fetch(
+          `${WEATHER_API_URL}/weather?q=${defaultCity}&appid=${WEATHER_API_KEY}&units=metric`
+        );
+
+        if (!currentWeatherResponse.ok) {
+          throw new Error(`HTTP error! Status: ${currentWeatherResponse.status}`);
+        }
+
+        const currentWeatherData = await currentWeatherResponse.json();
+
+        // Fetch forecast data for the default city
+        const forecastResponse = await fetch(
+          `${WEATHER_API_URL}/forecast?q=${defaultCity}&appid=${WEATHER_API_KEY}&units=metric`
+        );
+
+        if (!forecastResponse.ok) {
+          throw new Error(`HTTP error! Status: ${forecastResponse.status}`);
+        }
+
+        const forecastData = await forecastResponse.json();
+
+        // Set both current weather and forecast data
+        setCurrentWeather({ city: defaultCity, ...currentWeatherData });
+        setForecast({ city: defaultCity, ...forecastData });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [defaultCity]);
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
@@ -22,17 +60,21 @@ function App() {
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
-        const forcastResponse = await response[1].json();
+        const forecastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        setForecast({ city: searchData.label, ...forcastResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
       })
       .catch(console.log);
   };
-
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange} />
+      <img
+        alt="weather"
+        className="background-image"
+        src={`icons/${currentWeather.weather[0].icon}.jpg`}
+      />
+      <Search onSearchChange={handleOnSearchChange} defaultCity={defaultCity} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />}
     </div>
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { geoApiOptions, GEO_API_URL } from "../../api";
 
-const Search = ({ onSearchChange }) => {
-  const [search, setSearch] = useState(null);
+const Search = ({ onSearchChange, defaultCity }) => {
+  const [search, setSearch] = useState(defaultCity || null);
+
+  useEffect(() => {
+    setSearch(defaultCity || null);
+  }, [defaultCity]);
 
   const loadOptions = async (inputValue) => {
     try {
@@ -11,17 +15,17 @@ const Search = ({ onSearchChange }) => {
         `${GEO_API_URL}?minPopulation=1000000&namePrefix=${inputValue}`,
         geoApiOptions
       );
-  
+
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+
       const val = await response.json();
       const options = val.data.map((city) => ({
         value: `${city.latitude} ${city.longitude}`,
         label: `${city.name}, ${city.countryCode}`,
       }));
-  
+
       return { options };
     } catch (error) {
       console.log(error);
